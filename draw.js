e5d45a1e7dcb767f97790c2d903389c69c461846
Zack Stout <zackstout@gmail.com>
2018-02-24 17:59:50 -0600

    great, now we have added noise from p5, which was a bit trit tricky to integrate with browserify

--- draw.js
+++ draw.js
@@ -1,13 +1,15 @@
 
 var THREE = require('three');
 var OrbitControls = require('three-orbit-controls')(THREE);
+var p5 = require('./p5.js');
+// window.p5 = p5;
 
 var size = 2;
 var num = 15;
+var allSpheres = [];
 var color2 = new THREE.Color("rgb(0, 0, 255)");
 var material2 = new THREE.MeshLambertMaterial( { color: color2 } );
 var pos = new THREE.Vector3();
-
 
 var scene = new THREE.Scene();
 scene.background = new THREE.Color( 0xB0E0E6 );
@@ -15,46 +17,61 @@ var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeig
 var renderer = new THREE.WebGLRenderer({ alpha: true });
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
+var scale = 0.1;
+var inc = 500 / num;
 
-// Generate initial box:
-var geometry = new THREE.BoxGeometry( size, size, size );
-var cube = new THREE.Mesh( geometry, material2 );
-cube.receiveShadow = true;
-cube.castShadow = true;
-// scene.add( cube );
+var newp5 = new p5(function(sketch) {
 
+  sketch.setup = function() {
+    console.log('we in');
 
-for (var i=0; i < num; i++) {
-  for (var j=0; j < num; j++) {
-    for (var k=0; k < num; k++) {
-      var inc = 500 / num;
-      pos.set(i * inc, j * inc, k * inc);
-      var geom = new THREE.SphereGeometry( inc / 15 );
-      var sphere = new THREE.Mesh( geom, material2 );
-      sphere.position.copy(pos);
+    // Generate grid of spheres:
+    for (var i=0; i < num; i++) {
+      for (var j=0; j < num; j++) {
+        for (var k=0; k < num; k++) {
+          pos.set(i * inc, j * inc, k * inc);
 
-      //oh how strange, these are entirely necessary: (or at least one is)
-      sphere.receiveShadow = true;
-      sphere.castShadow = true;
-      scene.add(sphere);
+          // this is frustrating that i'm having trouble combining p5 and browserify:
+          var noiseVal = sketch.noise(scale * i * inc / num, scale * j * inc / num, scale * k * inc / num);
+
+          var geom = new THREE.SphereGeometry( inc / 15 );
+          var sphere = new THREE.Mesh( geom, material2 );
+          sphere.position.copy(pos);
+          sphere.noiseValue = noiseVal;
+
+          //oh how strange, these are entirely necessary: (or at least one is)
+          sphere.receiveShadow = true;
+          sphere.castShadow = true;
+          scene.add(sphere);
+
+          allSpheres.push(sphere);
+        }
+      }
     }
-  }
-}
 
-camera.position.x = 5 + size;
+    console.log(allSpheres);
 
-var controls = new OrbitControls( camera );
-controls.target.set( 0, 2, 0 );
-controls.update();
+    camera.position.x = 5 + size;
 
-var light = new THREE.PointLight(0xffffff);
-// Ok guess we just had to bring the light closer:
-light.position.set(size * 1.5, size * 1.5, size * 1.5);
-scene.add(light);
+    var controls = new OrbitControls( camera );
+    controls.target.set( 0, 2, 0 );
+    controls.update();
+
+    var light = new THREE.PointLight(0xffffff);
+    // Ok guess we just had to bring the light closer:
+    light.position.set(size * 1.5, size * 1.5, size * 1.5);
+    scene.add(light);
+
+  };
+
+
+}, 'dom-elem');
+
+
 
 function animate() {
-	requestAnimationFrame( animate );
-	renderer.render( scene, camera );
+  requestAnimationFrame( animate );
+  renderer.render( scene, camera );
 
 }
 
